ab31aaffce6e68b0286dee9ea22aa4694992fb0c
Nico <nicodiazan@gmail.com>
2023-04-22 10:43:47 +0200

    Add some basic dependencies and skeleton files

---
+++ src/pages/stockDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const stockDetailPage = () => {
+  return (
+    <div>stockDetailPage</div>
+  )
+}
+
+export default stockDetailPage
---
+++ src/pages/stockOverviewPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const stockOverviewPage = () => {
+  return (
+    <div>stockOverviewPage</div>
+  )
+}
+
+export default stockOverviewPage
