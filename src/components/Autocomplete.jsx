5aa2fa2439da38b8bf5ec353849f833bd948dd7b
Nico <nicodiazan@gmail.com>
2023-05-07 00:14:24 +0200

    Add data fetching on the search bar

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,27 +1,44 @@
 import React from "react";
 
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
 
 const Autocomplete = () => {
-    const [search, setSearch] = useState("")
+	const [search, setSearch] = useState("");
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+                const res = await finnHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+			} catch (err) {
+				console.warn("Failed to fetch query results");
+				console.warn(err);
+			}
+		};
+        fetchData()
+	}, search);
 	return (
 		<div className="w-50 p-5 rounded mx-auto">
 			<div className="form-floating dropdown">
 				<input
 					type="text"
 					style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
-                    id="search"
-                    className="form-control"
-                    placeholder="Search"
-                    autoComplete="off"
-                    value={search}
-                    onChange={setSearch(e.target.value)}
+					id="search"
+					className="form-control"
+					placeholder="Search"
+					autoComplete="off"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
 				/>
-                <label htmlFor="search">Search</label>
-                <ul className="dropdown-menu">
-                    <li>testStoc 1</li>
-                    <li>test stoc 2</li>
-                </ul>
+				<label htmlFor="search">Search</label>
+				<ul className="dropdown-menu">
+					<li>testStoc 1</li>
+					<li>test stoc 2</li>
+				</ul>
 			</div>
 		</div>
 	);
