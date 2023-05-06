fe8349399f2e886e7f40a3acb96c58ab40106fee
Nico <nicodiazan@gmail.com>
2023-05-06 22:29:30 +0200

    Add a proper table showing all important stats on stocks

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -29,26 +29,43 @@ const fetchAllData = async (symbols) => {
 const StockList = () => {
 	const [stocks, setStocks] = useState([]);
 	const [watchList, setWatchList] = useState(["MSFT", "AAPL", "AMZN"]);
-    
+
 	useEffect(() => {
-        let isMounted = true;
+		let isMounted = true;
 		isMounted && fetchAllData(watchList).then((res) => setStocks(res));
 		return () => {
 			isMounted = false;
 		};
 	}, []);
 
+
 	// fetchAllData(watchList);
 	return (
 		<>
-			<table>
+			<table className="table hover mt-5">
+				<thead style={{ color: "rgb(79, 89, 102)" }}>
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">Last</th>
+						<th scope="col">Change</th>
+						<th scope="col">Change%</th>
+						<th scope="col">High</th>
+						<th scope="col">Low</th>
+						<th scope="col">Open</th>
+						<th scope="col">Close</th>
+					</tr>
+				</thead>
 				<tbody>
 					{stocks.map(({ symbol: stock, data }) => (
-						<tr key={stock}>
+						<tr className="table-row" key={stock}>
 							<th>{stock}</th>
 							<td>{data.c}</td>
-							<td>data</td>
-							<td>here!</td>
+							<td className={`text-${data.d < 0 ? "danger" : "success"}`}>{data.d}</td>
+							<td className={`text-${data.dp < 0 ? "danger" : "success"}`}>{data.dp}%</td>
+							<td>{data.h}</td>
+							<td>{data.l}</td>
+							<td>{data.o}</td>
+							<td>{data.pc}</td>						
 						</tr>
 					))}
 				</tbody>
