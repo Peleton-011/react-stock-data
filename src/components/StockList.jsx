0624482ba5fdf33654cb5366cbf352d9eaca7ca7
Nico <nicodiazan@gmail.com>
2023-05-06 22:12:52 +0200

    Fix data fetching

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -4,12 +4,22 @@ import { useState, useEffect } from "react";
 import finnHub from "../apis/finnHub";
 
 const fetchData = async (symbol) => {
+	return finnHub.get("/quote", {
+		params: { symbol },
+	});
+};
+const fetchAllData = async (symbols) => {
 	try {
-		const res = await finnHub.get("/quote", {
-			params: { symbol },
-		});
-		console.log(res.data);
-		return res.data;
+		const res = await Promise.all(
+			symbols.map(async (symbol) => await fetchData(symbol))
+		);
+
+		const finalRes = res.map((response) => ({
+			symbol: response.config.params.symbol,
+			data: response.data,
+		}));
+		console.log(finalRes);
+		return finalRes;
 	} catch (error) {
 		console.warn("Failed to fetch data");
 		console.warn(error);
@@ -17,19 +27,26 @@ const fetchData = async (symbol) => {
 };
 
 const StockList = () => {
+	const [stocks, setStocks] = useState([]);
 	const [watchList, setWatchList] = useState(["MSFT", "AAPL", "AMZN"]);
+    
 	useEffect(() => {
-		fetchData();
+        let isMounted = true;
+		isMounted && fetchAllData(watchList).then((res) => setStocks(res));
+		return () => {
+			isMounted = false;
+		};
 	}, []);
-	fetchData("MSFT");
+
+	// fetchAllData(watchList);
 	return (
 		<>
 			<table>
 				<tbody>
-					{watchList.map((stock) => (
+					{stocks.map(({ symbol: stock, data }) => (
 						<tr key={stock}>
 							<th>{stock}</th>
-							<td>Some</td>
+							<td>{data.c}</td>
 							<td>data</td>
 							<td>here!</td>
 						</tr>
