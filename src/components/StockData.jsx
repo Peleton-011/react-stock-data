d3c53f23b5976446fcdef420b49dd049ce97b09c
Nico <nicodiazan@gmail.com>
2023-05-07 15:47:02 +0200

    Fix stock data display updates

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -42,35 +42,37 @@ const StockData = ({ symbol }) => {
 		],
 	];
 
-	const getElem = (stockData, dataFormat) => (
-		<div className="row border bg-white rounded shadow-sm p-4 mt-5">
-			{dataFormat.map((col, colIndex) => (
-				<div className="col" key={colIndex}>
-					{col.map((pair, rowIndex) => (
-						<div key={rowIndex}>
-							<span className="fw-bold">{pair[0]}:</span>
-							{pair[0] === "URL" ? (
-								<a
-									href={stockData[pair[1]]}
-									style={{ float: "right" }}
-								>
-									{stockData[pair[1]]}
-								</a>
-							) : (
-								<span style={{ float: "right" }}>
-									{stockData[pair[1]]}
-								</span>
-							)}
+	return (
+		<>
+			{stockData ? (
+				<div className="row border bg-white rounded shadow-sm p-4 mt-5">
+					{dataFormat.map((col, colIndex) => (
+						<div className="col" key={colIndex}>
+							{col.map((pair, rowIndex) => (
+								<div key={rowIndex}>
+									<span className="fw-bold">{pair[0]}:</span>
+									{pair[0] === "URL" ? (
+										<a
+											href={stockData[pair[1]]}
+											style={{ float: "right" }}
+										>
+											{stockData[pair[1]]}
+										</a>
+									) : (
+										<span style={{ float: "right" }}>
+											{stockData[pair[1]]}
+										</span>
+									)}
+								</div>
+							))}
 						</div>
 					))}
 				</div>
-			))}
-		</div>
+			) : (
+				<div>Loading...</div>
+			)}
+		</>
 	);
-
-    console.log(stockData)
-
-	return <>{stockData && getElem(stockData, dataFormat)}</>;
 };
 
 export default StockData;
