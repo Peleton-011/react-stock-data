b6e6243cdb810ce47fcac9472a6338a1bb9a114a
Nico <nicodiazan@gmail.com>
2023-05-07 15:05:02 +0200

    Fully implement stock data component

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import finnHub from "../apis/finnHub";
 
 const StockData = ({ symbol }) => {
+	const [stockData, setStockData] = useState();
 	let isMounted = true;
 	useEffect(() => {
 		const fetchData = async () => {
@@ -11,13 +12,63 @@ const StockData = ({ symbol }) => {
 				const res = await finnHub.get("/stock/profile2", {
 					params: { symbol },
 				});
+
+				if (isMounted) setStockData(res.data);
 			} catch (error) {
-                console.warn(error.message);
-            }
+				console.warn(error.message);
+			}
 		};
 		fetchData();
+		return () => {
+			isMounted = false;
+		};
 	}, [symbol]);
-	return <div>StockData</div>;
+
+	const dataFormat = [
+		[
+			["Name", "name"],
+			["Country", "country"],
+			["Ticker", "ticker"],
+		],
+		[
+			["Exchange", "exchange"],
+			["Industry", "finnhubIndustry"],
+			["IPO", "ipo"],
+		],
+		[
+			["Market Cap", "marketCapitalization"],
+			["Shares Outstanding", "shareOutstanding"],
+			["URL", "weburl"],
+		],
+	];
+
+	const getElem = (stockData, dataFormat) => (
+		<div className="row border bg-white rounded shadow-sm p-4 mt-5">
+			{dataFormat.map((col, colIndex) => (
+				<div className="col" key={colIndex}>
+					{col.map((pair, rowIndex) => (
+						<div key={rowIndex}>
+							<span className="fw-bold">{pair[0]}:</span>
+							{pair[0] === "URL" ? (
+								<a
+									href={stockData[pair[1]]}
+									style={{ float: "right" }}
+								>
+									{stockData[pair[1]]}
+								</a>
+							) : (
+								<span style={{ float: "right" }}>
+									{stockData[pair[1]]}
+								</span>
+							)}
+						</div>
+					))}
+				</div>
+			))}
+		</div>
+	);
+
+	return <>{stockData && getElem(stockData, dataFormat)}</>;
 };
 
 export default StockData;
