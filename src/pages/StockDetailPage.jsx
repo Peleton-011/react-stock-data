94a8d2d6bceb891499b73d6227ad798bd6c8ef4a
Nico <nicodiazan@gmail.com>
2023-05-07 12:16:50 +0200

    Add date checks to only fetch data from days in which the market is open

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import finnHub from "../apis/finnHub";
+import { BsChatLeftText } from "react-icons/bs";
 
 const StockDetailPage = () => {
 	const { symbol } = useParams();
@@ -12,8 +13,20 @@ const StockDetailPage = () => {
 		const fetchData = async () => {
             const date = new Date();
             //API needs it in seconds, getTime returns milliseconds
-            const currTime = Math.floor(date.getTime()/1000);
-            const dayInSeconds = 60 * 60 * 24
+            let currTime = Math.floor(date.getTime()/1000);
+            let dayInSeconds = 60 * 60 * 24
+
+            switch (date.getDay()) {
+                case 0: 
+                    currTime -= 2*dayInSeconds;
+                    break;
+                case 6: 
+                    currTime -= dayInSeconds;
+                    break;
+                default:
+                    break;
+            }
+
 			const res = await finnHub.get("/stock/candle", {
 				params: { symbol: symbol, from: currTime - dayInSeconds, to: currTime, resolution: 30 },
 			});
